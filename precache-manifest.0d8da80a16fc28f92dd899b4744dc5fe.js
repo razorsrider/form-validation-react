de095e0c93a5cd70a2bfcaf0bca8819ce183606a
razorsrider <razorsrider@gmail.com>
2020-07-12 16:44:38 +0400

    Updates

---
+++ precache-manifest.0d8da80a16fc28f92dd899b4744dc5fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb37c250396128decaa98d9718a7ab7",
+    "url": "/form-validation-react/index.html"
+  },
+  {
+    "revision": "020a16d94c21325b0e2d",
+    "url": "/form-validation-react/static/js/2.8b95f2f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/form-validation-react/static/js/2.8b95f2f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b5c409cbd136efb1c5",
+    "url": "/form-validation-react/static/js/main.6248bcba.chunk.js"
+  },
+  {
+    "revision": "9e7cb8adaf326e269b1a",
+    "url": "/form-validation-react/static/js/runtime-main.6322f0ec.js"
+  }
+]);
