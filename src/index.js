8abc55bbf07006ff0a544ac030a007c9665f3baa
razorsrider <razorsrider@gmail.com>
2020-07-12 17:07:04 +0400

    fixed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
-// import './App.css';
 import Form from './Form';
-
 
 class App extends Component {
     render() {
         return (
             <div className='App'>
                 <div className={'container-fluid'}>
-                    <div className={'row'}
-                         // style={{backgroundColor: 'white'}}
-                    >
-                        <div className={'col-6 offset-3'} >
+                    <div className={'row'}>
+                        <div className={'col-6 offset-3'}>
                             <h4 className={'mb-3'}>Информация о сотруднике</h4>
                             <Form/>
                         </div>
@@ -22,21 +18,6 @@ class App extends Component {
         )
     }
 }
-
-// const App = () => {
-//     return (
-//         <div>
-//             <div className={'container-fluid'}>
-//                 <div className={'row'} style={{backgroundColor: 'white'}}>
-//                     <div className={'col-6 offset-3'} style={{border: '1px solid'}}>
-//                         <h4>Информация о сотруднике</h4>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-
 
 ReactDOM
     .render(
