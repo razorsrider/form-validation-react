05e013c49f56bbb0f838a5979877765b8b48049e
razorsrider <razorsrider@gmail.com>
2020-08-01 19:06:30 +0400

    Updates

---
+++ precache-manifest.6550123240e2652d1c811fb118bb0586.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3b6abd9d98cf387cd8abc63521a9ae8",
+    "url": "/form-validation-react/index.html"
+  },
+  {
+    "revision": "5721c14ae91bd05572a6",
+    "url": "/form-validation-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fb45feca516f0c455935",
+    "url": "/form-validation-react/static/css/main.a5909b5a.chunk.css"
+  },
+  {
+    "revision": "5721c14ae91bd05572a6",
+    "url": "/form-validation-react/static/js/2.6015b48b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/form-validation-react/static/js/2.6015b48b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb45feca516f0c455935",
+    "url": "/form-validation-react/static/js/main.c078e95a.chunk.js"
+  },
+  {
+    "revision": "9e7cb8adaf326e269b1a",
+    "url": "/form-validation-react/static/js/runtime-main.6322f0ec.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b844a6ea95d614d5ffb244bdc471bf28.js"
+  "/form-validation-react/precache-manifest.6550123240e2652d1c811fb118bb0586.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/form-validation-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
